9404b78ac737b454b8c2264d6066bcb666be6006
PauloRicardoDev <pauloricardochagas.dev@gmail.com>
2024-12-06 21:44:27 -0300

    layout adjustments responsive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -85,4 +85,12 @@ export class HomeComponent implements OnInit {
     await this.authService.logout();
     this.router.navigate(['/login']);
   }
+
+
+  transform(value: string, maxLength: number = 15): string {
+    if (value.length > maxLength) {
+      return value.slice(0, maxLength) + "...";
+    }
+    return value;
+  }
 }
